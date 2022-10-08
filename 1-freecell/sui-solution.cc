411707ff38dbd90bdbe238a2502ea08747681954
Mylanos <marek@ziska.sk>
2022-10-08 14:02:50 +0200

    Distance of node from goal heuristic v1

--- 1-freecell/sui-solution.cc
+++ 1-freecell/sui-solution.cc
@@ -1,9 +1,13 @@
+// SUI Project 1
+// Authors: xpysik00, xziska03
+
 #include "search-strategies.h"
 
 #include "memusage.h"
 
 #include <queue>
 #include <algorithm>
+#include <numeric>
 
 
 typedef struct bfs_node {
@@ -143,14 +147,92 @@ double my_abs (double a, double b) {
 		return b - a;
 }
 
+// find the index of a given card
+int get_index(WorkStack workstack, int value, Color color)
+{
+	auto v = workstack.storage();
+    auto it = find_if(v.begin(), v.end(), [&value, &color](Card p) { return p.color == color && p.value == value; });
+    // if element was found
+    if (it != v.end()) 
+    {
+        // calculate index of a card
+        int index = it - v.begin();
+        return index + 1;
+    }
+    else {
+        // in case the card is not in given container
+        return 0;
+    }
+}
+
 double StudentHeuristic::distanceLowerBound(const GameState &state) const {
-	double val = 0;
+	int index = 0;
+	int sum = 0;
+	// matrice representing the card storages
+	std::vector<std::vector<int>> ones_stacks;
+	std::vector<Color> colors{Color::Heart, Color::Diamond, Color::Club, Color::Spade};
 
-	for (const auto &stack : state.stacks) {
-		val += stack.nbCards();
+	// create 2d representation of game state cards dimensions in stacks, filled with ones
+	for(auto &stack : state.stacks){
+		ones_stacks.push_back(std::vector<int>(stack.storage().size(), 1));
 	}
+	// value of currently evaluated card
+	int value = 1;
+	// stack column
+	int column = 0;
+	bool found_in_homes = false;
+	bool found_in_fc = false;
 
-    return val;
+	// evaluate positions of all cards
+	while(value<=king_value){
+		// for each color
+		for(auto color : colors){
+			// if free cells contain current card, add 1 to sum
+			for(auto &free_cell: state.free_cells){
+				auto fc_card = free_cell.topCard();
+				if(fc_card){
+					if (fc_card.value().value == value){
+						if(fc_card.value().color == color){
+							sum += 1;
+							found_in_fc = true;
+							break;
+						}
+					}
+				}
+				found_in_fc = false;
+			}
+			if(found_in_fc)
+				continue;
+			// if home cells contain current card, do nothing
+			for (auto &home : state.homes) {
+				auto top_card = home.topCard();
+				if(top_card){
+					if(color == top_card.value().color && value <= top_card.value().value){
+						found_in_homes = true;
+						break;
+					}
+				}
+				found_in_homes = false;
+			}
+			// if card was found in one of the homes, skip search in stacks
+			if(found_in_homes)
+				continue;
+			// search for index of given card
+			for (auto &stack : state.stacks) {
+				index = get_index(stack, value, color);
+				if(index){
+					// sum all values stored in the from found index
+					sum += std::accumulate(ones_stacks[column].begin() + index - 1, ones_stacks[column].end(), 0);
+					ones_stacks[column][index - 1] = 0;
+					break;
+				}
+				column++;
+			}
+			column = 0;
+		}
+		value++;
+	}
+    return (double)sum;
 }
 
 typedef struct a_star_node {
